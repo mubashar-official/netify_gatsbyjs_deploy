c8a782d67134a631a29f6c327ec456a93a5c9042
mubashar <mubashar.hussain22@gmail.com>
2022-01-28 09:11:04 +0500

    first deloyemtn

---
+++ gatsby-config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    siteMetadata: {
+      title: `netlify Gatsby`,
+        siteUrl: `https://www.yourdomain.tld`,
+    },
+    plugins: [
+
+    ]
+}
---
+++ src/pages/index.js
@@ -0,0 +1,12 @@
+import * as React from "react"
+
+// markup
+const IndexPage = () => {
+  return (
+    <>
+    <h1>Welcome to build gatsby JS delployment</h1>
+    </>
+  )
+}
+
+export default IndexPage
